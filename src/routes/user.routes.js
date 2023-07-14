61691766de94616f9e2d78b4cb984fea0d8d78aa
Luis Gabriel Janco <janco7249@gmail.com>
2023-07-14 13:56:49 -0400

    feat: add method to update user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,9 @@ import * as userCtrl from '../controllers/user.control.js'
 
 const router = Router()
 
-router.post('/auth', userCtrl.login)
-router.get('/:id', userCtrl.getUser)
-router.get('/', userCtrl.getUsers)
+router.post('/auth', userCtrl.login) // auth login
+router.get('/:id', userCtrl.getUser) // find user by id
+router.get('/', userCtrl.getUsers) // get all users
+router.put('/:id', userCtrl.updateUser) // update fields of user
 
 export default router
