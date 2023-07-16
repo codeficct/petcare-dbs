085fc25919adac5a504d77ac5be36df1dacd2ef0
Luis Gabriel Janco <janco7249@gmail.com>
2023-07-16 02:59:59 -0400

    feat: add route to user authentication

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,6 @@ import * as userCtrl from '../controllers/user.control.js'
 
 const router = Router()
 
-router.post('/auth', userCtrl.login) // auth login
 router.get('/:id', userCtrl.getUser) // find user by id
 router.get('/', userCtrl.getUsers) // get all users
 router.put('/:id', userCtrl.updateUser) // update fields of user
