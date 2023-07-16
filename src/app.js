61e2e5c5b139f442660e531b7ddaaf33774a5584
Luis Gabriel Janco <janco7249@gmail.com>
2023-07-16 03:00:47 -0400

    feat: set endpoints to auth and vaccine

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import userRoutes from './routes/user.routes.js'
 import petRoutes from './routes/pet.routes.js'
+import authRoutes from './routes/auth.routes.js'
+import vaccineRoutes from './routes/vaccine.routes.js'
 
 const app = express()
 
@@ -11,7 +13,9 @@ app.use(cors({ origin: ['http://localhost:3000'], optionsSuccessStatus: 200 }))
 
 app.use(express.json())
 
+app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/pet', petRoutes)
+app.use('/api/vaccine', vaccineRoutes)
 
 export default app
