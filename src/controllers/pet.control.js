3a5601fb9189a9ea46d391fe02b172a0442e2cc7
Luis Gabriel Janco <janco7249@gmail.com>
2023-07-16 02:53:35 -0400

    refactor: get a pet depending on the role

--- src/controllers/pet.control.js
+++ src/controllers/pet.control.js
@@ -1,10 +1,13 @@
 import PetModel from '../models/pet.js'
+import UserModel from '../models/user.js'
 
 export const craetePet = async (req, res) => {
   try {
-    console.log(req.body)
     const newPet = new PetModel(req.body)
     await newPet.save()
+    await UserModel.findByIdAndUpdate(req.body.owner, {
+      $push: { pets: [newPet._id] }
+    })
     res.sendStatus(201)
   } catch (error) {
     console.log(error.message)
@@ -14,24 +17,23 @@ export const craetePet = async (req, res) => {
 
 export const getPet = async (req, res) => {
   try {
-    let pet = null
+    let pet
     if (req.params.role === 'vet') {
-      pet = await PetModel.findById(req.params.id).populate({
-        path: 'owner', select: 'name photo email'
-      })
-    } else {
-      const haveVaccination = await PetModel.findById(req.params.id)
-      if (haveVaccination.vaccines.length > 0) {
-        pet = await PetModel.findById(req.params.id).populate({
+      pet = await PetModel.findById(req.params.id)
+        .populate({
+          path: 'owner', select: 'name photo email'
+        })
+        .populate({
           path: 'vaccines', select: 'nameVaccine dateVaccine'
         })
-      } else {
-        pet = await PetModel.findById(req.params.id)
-      }
+    } else {
+      pet = await PetModel.findById(req.params.id).populate({
+        path: 'vaccines', select: 'nameVaccine dateVaccine'
+      })
     }
     res.status(200).json(pet)
   } catch (error) {
-    console.log(error)
+    console.log(error.message)
     res.sendStatus(406)
   }
 }
@@ -43,6 +45,10 @@ export const getAllPets = async (req, res) => {
 
 export const deletePet = async (req, res) => {
   try {
+    const pet = await PetModel.findById(req.params.id)
+    await UserModel.findByIdAndUpdate(pet.owner, {
+      $pull: { pets: [req.params.id] }
+    })
     await PetModel.findByIdAndDelete(req.params.id)
     res.sendStatus(404)
   } catch (error) {
